90c810407174b865ab325b6e3bbeedfdf02ced97
Dominique Feyer <dfeyer@ttree.ch>
2017-11-09 22:44:06 +0100

    TASK: Initial commit

---
+++ Classes/MediaHelper.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Ttree\Eel\Media;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Eel\ProtectedContextAwareInterface;
+use Neos\Media\Domain\Model\Image;
+use Neos\Media\Domain\Model\ThumbnailConfiguration;
+use Neos\Media\Domain\Service\AssetService;
+
+/**
+ * @Flow\Proxy(false)
+ */
+class MediaHelper implements ProtectedContextAwareInterface
+{
+    /**
+     * @var AssetService
+     * @Flow\Inject
+     */
+    protected $assetService;
+
+    public function imageUri(Image $asset, $width = null, $maximumWidth = null, $height = null, $maximumHeight = null, $allowCropping = false, $allowUpScaling = false) :string
+    {
+        $thumbnailConfiguration = new ThumbnailConfiguration($width, $maximumWidth, $height, $maximumHeight, $allowCropping, $allowUpScaling);
+        $thumbnailData = $this->assetService->getThumbnailUriAndSizeForAsset($asset, $thumbnailConfiguration);
+        if ($thumbnailData === null) {
+            return '';
+        }
+        return $thumbnailData['src'];
+    }
+
+    public function allowsCallOfMethod($methodName)
+    {
+        return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# A basic EEL helper to generate image URI
+
+Generate square cropped image:
+
+	${Ttree.Media.image(image, null, 100, null, 100, true)}
+	
+
+## Acknowledgments
+
+Development sponsored by [ttree ltd - neos solution provider](http://ttree.ch).
+
+We try our best to craft this package with a lots of love, we are open to sponsoring, support request, ... just contact us.
+
+## License
+
+The MIT License (MIT). Please see [LICENSE](LICENSE) for more information.
